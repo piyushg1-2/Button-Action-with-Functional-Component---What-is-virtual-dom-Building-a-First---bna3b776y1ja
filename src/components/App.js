1e64ea3e7e5dc96eacc1bd8280cdf3087e88f08e
piyushg1-2 <guptapiyush942@gmail.com>
2022-10-18 18:11:28 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+var txt="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
 function App() {
+  const [str,setStr]=useState("");
+
+   function show(){
+     setStr(txt);
+   }
   return (
     <div id="main">
-      // Do not alter the main div
+     <p id="para">{str}</p>
+      <button type="button" id="click" onClick={show}></button>
     </div>
   );
 }
